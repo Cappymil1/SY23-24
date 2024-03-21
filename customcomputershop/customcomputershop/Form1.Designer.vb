c0f333bbab8aeac606bbab9c26af8a3eb0725aac
Will Miller <womil709@learner.dvusd.org>
2024-03-21 13:42:00 -0700

    random

--- customcomputershop/customcomputershop/Form1.Designer.vb
+++ customcomputershop/customcomputershop/Form1.Designer.vb
@@ -22,10 +22,103 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Totalcosttimer = New System.Windows.Forms.Timer(Me.components)
+        Me.caseoption1 = New System.Windows.Forms.Button()
+        Me.case1labelamount = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Controls.Add(Me.case1labelamount)
+        Me.GroupBox1.Controls.Add(Me.caseoption1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 28)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(238, 209)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Computer shell."
+        '
+        'Label1
+        '
+        Me.Label1.AutoEllipsis = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(195, 324)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(143, 34)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Total cost"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(217, 358)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 32)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "$0.00"
+        '
+        'Totalcosttimer
+        '
+        Me.Totalcosttimer.Interval = 1
+        '
+        'caseoption1
+        '
+        Me.caseoption1.Location = New System.Drawing.Point(17, 38)
+        Me.caseoption1.Name = "caseoption1"
+        Me.caseoption1.Size = New System.Drawing.Size(75, 23)
+        Me.caseoption1.TabIndex = 0
+        Me.caseoption1.Text = "Option 1"
+        Me.caseoption1.UseVisualStyleBackColor = True
+        '
+        'case1labelamount
+        '
+        Me.case1labelamount.AutoSize = True
+        Me.case1labelamount.Location = New System.Drawing.Point(120, 41)
+        Me.case1labelamount.Name = "case1labelamount"
+        Me.case1labelamount.Size = New System.Drawing.Size(14, 16)
+        Me.case1labelamount.TabIndex = 1
+        Me.case1labelamount.Text = "0"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(17, 78)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(97, 20)
+        Me.CheckBox1.TabIndex = 2
+        Me.CheckBox1.Text = "CheckBox1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Totalcosttimer As Timer
+    Friend WithEvents caseoption1 As Button
+    Friend WithEvents case1labelamount As Label
+    Friend WithEvents CheckBox1 As CheckBox
 End Class
--- customcomputershop/customcomputershop/Form1.vb
+++ customcomputershop/customcomputershop/Form1.vb
@@ -1,3 +1,18 @@
 ﻿Public Class Form1
+    Dim casecost As Decimal
 
+    Private Sub caseoption1_Click(sender As Object, e As EventArgs) Handles caseoption1.Click
+        Dim c1 As Integer
+
+        case1labelamount.Text = c1 + 1
+
+
+        casecost = casecost + 350
+        Label2.Text = casecost.ToString("c2")
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        casecost = casecost + 350
+        Label2.Text = casecost.ToString("c2")
+    End Sub
 End Class
