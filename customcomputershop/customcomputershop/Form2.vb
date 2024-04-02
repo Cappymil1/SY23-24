74940c8b826ef9ec3eba37b818ff0517e3eb03de
Will Miller <womil709@learner.dvusd.org>
2024-04-02 13:42:52 -0700

    computershopmore
    d

--- customcomputershop/customcomputershop/Form2.vb
+++ customcomputershop/customcomputershop/Form2.vb
@@ -1,7 +1,8 @@
 ﻿Public Class Form2
-    Dim f2
-    Dim totalcost As Decimal
-    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        costlabel.Text = Module1.totalcost
+        totalaftertax.Text = Module1.totalcost * 1.133 / 100.ToString("c2")
     End Sub
+
 End Class
